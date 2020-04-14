7a7d51c70e8cf13c6ad0987c078bad597de4a7e3
Sanidhya Mangal <mangalsanidhya19@gmail.com>
2020-04-14 23:34:31 +0530

    added: predictions and trained models
    feat: added api's for pneumonia and malaria detection
    
    updated: HTML template for uploading files

--- app.py
+++ app.py
@@ -1,11 +1,69 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, jsonify
+import base64
+from predictions import predict_malaria, predict_pneumonia
+import uuid
+
+
 
 app = Flask(__name__)
+UPLOAD_FOLDER = './uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET'])
 def home():
     return render_template("index.html")
 
+@app.route('/predict/malaria', methods=['POST', 'GET'])
+def malaria():
+    if request.method == 'POST':
+            # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            result_response = {}
+            result = predict_malaria(base64.b64encode(file.read()))
+            if result:
+                result_response["result"] = "True"
+            else:
+                result_response["result"] = "False"
+            return jsonify(result_response)
+        return redirect("/")
+
+
+@app.route('/predict/pneumonia', methods=['POST', 'GET'])
+def pneumonia():
+    if request.method == 'POST':
+            # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            result_response = {}
+            result = predict_pneumonia(base64.b64encode(file.read()))
+            if result:
+                result_response["result"] = "True"
+            else:
+                result_response["result"] = "False"
+            return jsonify(result_response)
+        return redirect("/")
 
 
 if __name__ == "__main__":
---
+++ predictions.py
@@ -0,0 +1,50 @@
+import base64  # for handling base64 images
+import os  # for performing os relatec work
+import tensorflow as tf  # for deep learning
+
+
+# function to malaria
+def predict_malaria(base64image) -> bool:
+    """
+    A function to predict whether microscope image has malaria bacteria or not
+    :param base64image: an image in base64 format
+    :return: a bool value wether malaria is detected or not.
+    """
+    # load malaria model
+    
+    model = tf.keras.models.load_model('./trained_models/cnn_malaria.h5')
+
+    # get image path and open it using tf
+    img_decode = tf.image.decode_image(
+        base64.b64decode(base64image.split(';base64,').pop()))
+    # resize image based on model arch
+    image_to_predict = tf.image.resize(img_decode, size=[
+        150, 150
+    ]) / 255.0  # normalize img by dividing by 255.0
+
+    # predict image
+    return model.predict(tf.reshape(image_to_predict, shape=[-1, 150, 150, 3
+                                                             ])) < 0
+
+
+# function to predict pneumonia
+def predict_pneumonia(base64image) -> bool:
+    """
+    A function to predict whether an x ray image is having pneumonia or not
+    :param base64image:
+    :return: bool value about whether pneumonia is detected or not
+    """
+    img_decode = tf.image.decode_image(
+        base64.b64decode(base64image))
+
+    # resize image based on model arch
+    image_to_predict = tf.image.resize(img_decode, size=[
+        150, 150
+    ]) / 255.0  # normalize img by dividing by 255.0
+
+    cwd = os.path.dirname(__file__)
+    model = tf.keras.models.load_model(
+        os.path.join(cwd, './trained_models/cnn_pneumonia.h5'))
+
+    return model.predict(tf.reshape(image_to_predict, shape=[-1, 150, 150, 1
+                                                             ])) > 0.75
